ca69b589206d1b5f514f4c86d5e6f20ec0d29267
blendi-remade <blendi@remade.ai>
2025-12-07 21:04:10 -0800

    refactor: replace ExampleMod with Falcraft and remove unused mixins
    - Deleted ExampleMod.java and ExampleMixin.java, consolidating functionality into Falcraft.java.
    - Updated fabric.mod.json to reference the new Falcraft class.
    - Cleared mixin configurations in falcraft.mixins.json as no mixins are currently used.

---
+++ src/main/java/com/falcraft/Falcraft.java
@@ -0,0 +1,17 @@
+package com.falcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Falcraft implements ModInitializer {
+	public static final String MOD_ID = "falcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Falcraft initialized");
+	}
+}
+
